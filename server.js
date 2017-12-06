aea2698e87ea62d2fc38e8f150874471c0093c17
Mandy Tong <mandytong@Mandys-MacBook-Pro.local>
2017-12-05 22:40:16 -0800

    cleaned up user login

--- server.js
+++ server.js
@@ -2,10 +2,13 @@
 const express  = require('express');
 const mongoose = require('mongoose');
 const morgan   = require('morgan');
+const methodOverride = require('method-override');
 const session  = require('express-session');
 const bcrypt 	 = require('bcrypt');
 const app      = express();
+require('pretty-error').start();
 const PORT     = process.env.PORT || 3000;
+
 
 // process.env.MONGOB_URI
 // connect to database
@@ -13,6 +16,7 @@ const mongoURI = process.env.MONGODB_URI || 'mongodb://localhost:27017/cool-kick
 
 mongoose.connect(mongoURI, { useMongoClient: true});
 mongoose.Promise = global.Promise;
+
 
 // test database connection
 const db = mongoose.connection;
@@ -25,22 +29,25 @@ app.use(express.json());
 app.use(express.static('public'));
 app.use(morgan('dev'));
 
-secret: "akjfdskfjdsfjsfk", //random string here
-resave: false,
-saveUninitialized: false
+app.use(session({
+  secret: 'kajfkdsfhdis', //random string here
+  resave: false,
+  saveUninitialized: false
 }));
+
 
 // controllers
 const sneakersController = require('./controllers/sneakers.js');
 const commentsController = require('./controllers/comments.js');
 const sessionsController = require('./controllers/session.js');
 
+app.use(methodOverride('_method'));
 app.use('/sneakers', sneakersController);
 app.use('/comments', commentsController);
 app.use('/user', sessionsController);
 
 // root route
-app.get('/', (req, res) => res.redirect('/sneakers'));
+app.get('/', (req, res) => res.redirect('/sneakers/home'));
 
 
 
